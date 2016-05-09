93011b2a59b1442717fd35bc71c4f707bc6804aa
Cristiano <cristianocortez@gmail.com>
2016-05-09 17:28:52 -0300

    Preparação do parser JSON

--- app/src/main/java/com/authorwjf/http_get/Main.java
+++ app/src/main/java/com/authorwjf/http_get/Main.java
@@ -2,6 +2,9 @@ package com.authorwjf.http_get;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -9,15 +12,39 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Main extends Activity implements OnClickListener {
+
+	// contacts JSONArray
+	JSONArray contacts = null;
+
+	// JSON Node names
+	private static final String TAG_CONTACTS = "contacts";
+	private static final String TAG_ID = "id";
+	private static final String TAG_NAME = "name";
+	private static final String TAG_EMAIL = "email";
+	private static final String TAG_ADDRESS = "address";
+	private static final String TAG_GENDER = "gender";
+	private static final String TAG_PHONE = "phone";
+	private static final String TAG_PHONE_MOBILE = "mobile";
+	private static final String TAG_PHONE_HOME = "home";
+	private static final String TAG_PHONE_OFFICE = "office";
+
+	// Hashmap for ListView
+	ArrayList<HashMap<String, String>> contactList;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,13 +79,58 @@ public class Main extends Activity implements OnClickListener {
 			 HttpContext localContext = new BasicHttpContext();
              //HttpGet httpGet = new HttpGet("http://www.cheesejedi.com/rest_services/get_big_cheese.php?puzzle=1");
 			 //HttpGet httpGet = new HttpGet("http://localhost/testews.php");
-			 HttpGet httpGet = new HttpGet("http://192.168.96.9/testews.php");
+			 //HttpGet httpGet = new HttpGet("http://192.168.96.9/testews.php");
+			 HttpGet httpGet = new HttpGet("http://api.androidhive.info/contacts/");
 			// [{"id":"59","level":"1","time_in_secs":"4","par":"0","initials":"TNF","quote":"Hey hey ole hickery", "time_stamp":"2012-03-03 04:36:15"}]
              String text = null;
              try {
                    HttpResponse response = httpClient.execute(httpGet, localContext);
                    HttpEntity entity = response.getEntity();
                    text = getASCIIContentFromEntity(entity);
+
+
+				 if (text != null) {
+					 try {
+						 JSONObject jsonObj = new JSONObject(text);
+
+						 // Getting JSON Array node
+						 contacts = jsonObj.getJSONArray(TAG_CONTACTS);
+
+						 // looping through All Contacts
+						 for (int i = 0; i < contacts.length(); i++) {
+							 JSONObject c = contacts.getJSONObject(i);
+
+							 String id = c.getString(TAG_ID);
+							 String name = c.getString(TAG_NAME);
+							 String email = c.getString(TAG_EMAIL);
+							 String address = c.getString(TAG_ADDRESS);
+							 String gender = c.getString(TAG_GENDER);
+
+							 // Phone node is JSON Object
+							 JSONObject phone = c.getJSONObject(TAG_PHONE);
+							 String mobile = phone.getString(TAG_PHONE_MOBILE);
+							 String home = phone.getString(TAG_PHONE_HOME);
+							 String office = phone.getString(TAG_PHONE_OFFICE);
+
+							 // tmp hashmap for single contact
+							 HashMap<String, String> contact = new HashMap<String, String>();
+
+							 // adding each child node to HashMap key => value
+							 contact.put(TAG_ID, id);
+							 contact.put(TAG_NAME, name);
+							 contact.put(TAG_EMAIL, email);
+							 contact.put(TAG_PHONE_MOBILE, mobile);
+
+							 // adding contact to contact list
+							 contactList.add(contact);
+						 }
+					 } catch (JSONException e) {
+						 e.printStackTrace();
+					 }
+				 } else {
+					 Log.e("ServiceHandler", "Couldn't get any data from the url");
+				 }
+
              } catch (Exception e) {
             	 return e.getLocalizedMessage();
              }
